b43384e97516b9a1c24778fa5e8aebe4334d56db
Toma400 <thomasso_calgori@o2.pl>
2025-10-25 20:17:12 +0200

    Swapped .nim zipper to .py one because I can't explain how much `zippy` frustrated me

---
+++ zip/pack.py
@@ -0,0 +1,33 @@
+import zipfile
+import os
+
+FILES = [
+    "gui/font.png",
+    "gui/font.png.dat",
+    "gui/grid.png",
+    "gui/gui.png",
+    "maps/default.olm",    # those to be replaced later
+    "maps/default2.olm",
+    "maps/tamriel.olm",        # temporary TES stuff
+    # "maps/summerset.olm",
+    "maps/tiled.js",
+    "maps/tiled.md",
+    "tilesets/example.oldata",
+    "tilesets/example.png",
+    "tilesets/locs.oldata",
+    "tilesets/locs.png",
+    "tilesets/ptr_locs.oldata", # temporary TES stuff
+    "tilesets/ptr_locs.png",    # temporary TES stuff
+    "tilesets/system.png",
+    "OfLands.exe",
+    "oflands.ini",
+    "SDL2.dll"
+]
+
+os.system("compile.bat")
+
+with zipfile.ZipFile("../OfLands.zip", mode="w") as archive:
+    for f in FILES:
+        with archive.open(f, "w") as fw:
+            with open(f"../{f}", "rb") as fr:
+                fw.write(fr.read())
